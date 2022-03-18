02f9b027fe4378c91a67f2488d3063eb3a629cd6
Shibo Xing <shx26@pitt.edu>
2022-03-17 20:41:04 -0400

    feat: add hw code

--- hw/test.py
+++ hw/test.py
@@ -1,3 +1,4 @@
+from re import L
 import matplotlib.pyplot as plt
 from bisect import bisect, bisect_left
 
@@ -11,29 +12,48 @@ with open("scores") as f:
 print(scores1)
 print(scores2)
 
+scores = []
+for s, l in scores2:
+    if l:
+        scores += [(s, l)] * 10
+    else:
+        scores.append((s, l))
+
 step = 1
 TPR, FPR = [], []
+PRECISION = []
 for i in range(10):
     tp, fn, tn, fp = 0, 0, 0, 0
-    for j in range(len(scores1)):
-        if scores1[j][0] >= i:
-            if scores1[j][1]:
+    for j in range(len(scores)):
+        if scores[j][0] >= i:
+            if scores[j][1]:
                 tp += 1
             else:
                 fp += 1
         else:
-            if scores1[j][1]:
+            if scores[j][1]:
                 fn += 1
             else:
                 tn += 1
     tpr = float(tp) / (tp + fn)
     fpr = float(fp) / (fp + tn)
-    # print(tp, fn, tn, fp)
-    # print(tpr, fpr)
+    precision = float(tp) / (tp+fp) if tp+fp else 0
     TPR.append(tpr)
     FPR.append(fpr)
-plt.plot(FPR, TPR)
+    PRECISION.append(precision)
+    if i==3 or i==5 or i==7:
+        print(tp, fn, tn, fp)
+        print(tpr, fpr)
+        print('precision: ', precision)
+        print('f-measure: ', 2.0/(1/precision + 1/tpr), 2*tp/(2*tp + fp + fn))
+
+fig, ax = plt.subplots()
+ax.plot(FPR, TPR)
 plt.ylabel("TPR")
 plt.xlabel("FPR")
+for i in [3, 5, 7]:
+    # ax.scatter(FPR[i], TPR[i])
+    ax.scatter(PRECISION[i], TPR[i])
+    ax.annotate(f"threshold: {i}", (FPR[i], TPR[i]))
 plt.rc('font', size=15)
 plt.show()
